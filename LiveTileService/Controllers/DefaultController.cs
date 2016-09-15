ff6688a2fc8b6bb9b72f391fc8ef0aa18a3a1f10
Rogger Matamoros <roggmatz@gmail.com>
2016-09-15 14:42:17 -0400

    Added logic to populate the View with the data retrieved from OpenWeatherMaps.

--- LiveTileService/Controllers/DefaultController.cs
+++ LiveTileService/Controllers/DefaultController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,8 +10,17 @@ namespace LiveTileService.Controllers
     public class DefaultController : Controller
     {
         // GET: Default
-        public ActionResult Index()
+        public async Task<ActionResult> Index(string lat, string lon)
         {
+            //Let us validate
+            double latidude = double.Parse(lat.Substring(0, 5));
+            double longitude = double.Parse(lon.Substring(0, 5));
+
+            var weather = await Models.OpenWeatherMapProxy.GetWeather(latidude, longitude);
+
+            ViewBag.Name = weather.name;
+            ViewBag.Description = weather.weather[0].description;
+            ViewBag.Temp = ((int)weather.main.temp).ToString();
             return View();
         }
     }
